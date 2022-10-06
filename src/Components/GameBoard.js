53ba6a280be3e6af453e4c2c731208d8275ff0c8
Vaisakh Nair <vbnair988@gmail.com>
2022-10-06 11:39:55 +0200

    first commit

---
+++ src/Components/GameBoard.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import '../Game.css';
+import GameCircle from "./GameCircle";
+
+const NO_CIRCLES = 16;
+const NO_PLAYER = 0;
+const PLAYER_1 = 1;
+const PLAYER_2= 2;
+
+const GameBoard = () =>{
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
+    const [currentPlayer, setcurrentPlayer] = useState(PLAYER_1);
+
+    console.log(gameBoard);
+
+    // Add Circles 
+    const initBoard = () => {
+        const circles = [];
+
+        for (let i=0; i < NO_CIRCLES; i++){
+            circles.push(renderCircle(i));
+        }
+        return circles;
+    }
+
+    const circleClicked = (id) =>{
+        console.log('circle clicked: ' + id);
+
+
+
+        setGameBoard(prev =>{
+            return prev.map((circle, pos)=>{
+                if (pos === id ) return currentPlayer;
+                return circle;
+            })
+        })
+
+        setcurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
+
+        console.log(gameBoard);
+        console.log(currentPlayer);
+    }
+
+const renderCircle = id =>{
+    return <GameCircle key={id} id={id} className={`player_${gameBoard[id]}`} onCircleClicked={circleClicked} />
+}
+
+    return (
+    <div className="gameBoard">
+    {initBoard()}
+    </div>    
+    )
+}
+
+export default GameBoard;
